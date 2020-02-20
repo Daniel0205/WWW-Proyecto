d94d53836f5fbdae2e66fad747b6d5fc7348a042
daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2020-02-20 17:49:37 -0500

    modelo acabado

--- Backend/server/facturas/models.py
+++ Backend/server/facturas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    id =  models.IntegerField(primary_key=True)
+    name = models.TextField(null=True)
+    apellidos = models.TextField(null=True)
+    email = models. EmailField(null=True,max_length=254)
+
+
+    TYPE_CHOICES = {
+        ("N", "natural"),
+        ("j", "juridica"),
+    }
+
+    type = models.CharField(null=True,max_length=1, choices=TYPE_CHOICES,
+        default="N")
+  
+
+    WAY_CHOICES = {
+        ("L", "Linea"),
+        ("F", "Fisico"),
+        ("E", "Email"),
+    }
+    shipping_way = models.CharField(null=True,max_length=1, choices=WAY_CHOICES,
+        default="L")
