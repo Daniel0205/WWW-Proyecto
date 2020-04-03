c0f8beb5c7c2e8e5f76f59fbfcc84533f3b09e59
jem <jem.suarez@correounivalle.edu.co>
2020-04-02 20:34:39 -0500

    CRUD de banco ajustado al nuevo modelo del backend y mensajes de notificación añadidos

--- backend/facturas/urls.py
+++ backend/facturas/urls.py
@@ -13,10 +13,40 @@ from .views import (
    ClientUpdateView,
    ClientListView,
 
+   #Apartment
+   ApartmentCreateView,
+   ApartmentUpdateView,
+   ApartmentListView,
+
+   #Substation
+   SubstationCreateView,
+   SubstationUpdateView,
+   SubstationListView,
+
+   #Transformer
+   TransformerCreateView,
+   TransformerUpdateView,
+   TransformerListView,
+
+   #ElectricityMeter
+   ElectricityMeterCreateView,
+   ElectricityMeterUpdateView,
+   ElectricityMeterListView,
+
    #Bank
    BankCreateView,
    BankUpdateView,
    BankListView,
+
+   #Bill
+   BillCreateView,
+   BillUpdateView,
+   BillListView,
+
+   #Payment
+   PaymentCreateView,
+   PaymentUpdateView,
+   PaymentListView,
 
 )
 
@@ -34,9 +64,40 @@ urlpatterns = [
     #User
     path('user', UserListView.as_view()),
     path('user/update/<pk>',UserUpdateView.as_view()),
-    
-    #User
+
+    #Apartments
+    path('apartment', ApartmentListView.as_view()),
+    path('apartment/update/<pk>',ApartmentUpdateView.as_view()),
+    path('apartment/create/',ApartmentCreateView.as_view()),
+
+    #Substation
+    path('substation', SubstationListView.as_view()),
+    path('substation/update/<pk>',SubstationUpdateView.as_view()),
+    path('substation/create/',SubstationCreateView.as_view()),
+
+    #Transformer
+    path('transformer', TransformerListView.as_view()),
+    path('transformer/update/<pk>',TransformerUpdateView.as_view()),
+    path('transformer/create/',TransformerCreateView.as_view()),
+
+    #ElectricityMeter
+    path('electricitymeter', ElectricityMeterListView.as_view()),
+    path('electricitymeter/update/<pk>',ElectricityMeterUpdateView.as_view()),
+    path('electricitymeter/create/',ElectricityMeterCreateView.as_view()),
+
+    #Bank
     path('bank', BankListView.as_view()),
     path('bank/update/<pk>',BankUpdateView.as_view()),
     path('bank/create/',BankCreateView.as_view()),
+
+    #Bill
+    path('bill', BillListView.as_view()),
+    path('bill/update/<pk>',BillUpdateView.as_view()),
+    path('bill/create/',BillCreateView.as_view()),
+
+    #Payment
+    path('payment', PaymentListView.as_view()),
+    path('payment/update/<pk>',PaymentUpdateView.as_view()),
+    path('payment/create/',PaymentCreateView.as_view()),
+
 ]
