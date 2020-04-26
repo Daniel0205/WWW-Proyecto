788771f172926d59b23c12b5797c0b0cfe2e7f62
jem <jem.suarez@correounivalle.edu.co>
2020-04-26 10:49:29 -0500

    frontend/src/__tests__ creado y añadidas pruebas unitarias basicas para el commponente Banks

--- frontend/src/components/Dashboard/Content/Banks.js
+++ frontend/src/components/Dashboard/Content/Banks.js
@@ -1,39 +1,39 @@
 import React from "react";
 import MaterialTable from "material-table";
 import axios from "axios";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Snackbar from "@material-ui/core/Snackbar";
 import SnackbarMesssages from "../../SnackbarMesssages";
 
-const styles = theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     maxWidth: 936,
     margin: "auto",
-    overflow: "hidden"
+    overflow: "hidden",
   },
   searchBar: {
-    borderBottom: "1px solid rgba(0, 0, 0, 0.12)"
+    borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
   },
   searchInput: {
-    fontSize: theme.typography.fontSize
+    fontSize: theme.typography.fontSize,
   },
   block: {
-    display: "block"
+    display: "block",
   },
   addUser: {
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   contentWrapper: {
-    margin: "40px 16px"
-  }
-});
+    margin: "40px 16px",
+  },
+}));
 
-function Banks(props) {
+export function Banks() {
   axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
   axios.defaults.xsrfCookieName = "csrftoken";
   axios.defaults.headers = {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   };
 
   const [state, setState] = React.useState({
@@ -44,15 +44,15 @@ function Banks(props) {
         editable: "onAdd",
         type: "numeric",
         cellStyle: { textAlign: "center" },
-        headerStyle: { textAlign: "center" }
+        headerStyle: { textAlign: "center" },
       },
       { title: "Name", field: "name" },
       {
         title: "City",
         field: "city",
 
-        initialEditValue: "Cali",
-        lookup: { C: "Cali", B: "Bogota", M: "Medellin" }
+        initialEditValue: "C",
+        lookup: { C: "Cali", B: "Bogota", M: "Medellin" },
       },
       {
         title: "Active",
@@ -60,35 +60,35 @@ function Banks(props) {
         initialEditValue: "true",
         lookup: { true: "True", false: "False" },
         cellStyle: { textAlign: "left", width: 10, maxWidth: 10 },
-        headerStyle: { textAlign: "left", width: 10, maxWidth: 10 }
-      }
+        headerStyle: { textAlign: "left", width: 10, maxWidth: 10 },
+      },
     ],
-    data: []
+    data: [],
   });
 
   React.useEffect(() => {
     axios
       .get("http://localhost:8000/api/bank")
-      .then(response => {
+      .then((response) => {
         setState({
           columns: state.columns,
-          data: response.data.map(x => {
+          data: response.data.map((x) => {
             return {
               id: x.id_bank,
               name: x.name_bank,
               active: x.active,
-              city: x.city_bank
+              city: x.city_bank,
             };
-          })
+          }),
         });
         console.log(response);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  const { classes } = props;
+  const classes = useStyles();
   const [messaje, setMessaje] = React.useState("");
   const [type, setType] = React.useState("");
   const [open, setOpen] = React.useState(false);
@@ -102,18 +102,18 @@ function Banks(props) {
 
   return (
     <>
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} data-testid="banks-paper">
         <MaterialTable
           style={{
             //backgroundColor: "#ddd",
-            padding: "0px 15px"
+            padding: "0px 15px",
           }}
           title="Banks"
           columns={state.columns}
           data={state.data}
           editable={{
-            onRowAdd: newData =>
-              new Promise(resolve => {
+            onRowAdd: (newData) =>
+              new Promise((resolve) => {
                 setTimeout(() => {
                   resolve();
 
@@ -122,11 +122,11 @@ function Banks(props) {
                       id_bank: newData.id,
                       name_bank: newData.name,
                       city_bank: newData.city,
-                      active: newData.active
+                      active: newData.active,
                     })
-                    .then(response => {
+                    .then((response) => {
                       console.log(response);
-                      setState(prevState => {
+                      setState((prevState) => { 
                         const data = [...prevState.data];
                         data.push(newData);
                         //Message Alert
@@ -137,7 +137,7 @@ function Banks(props) {
                         return { ...prevState, data };
                       });
                     })
-                    .catch(error => {
+                    .catch((error) => {
                       //Message Alert
                       setType("error");
                       setMessaje("All fields are required");
@@ -148,7 +148,7 @@ function Banks(props) {
                 }, 600);
               }),
             onRowUpdate: (newData, oldData) =>
-              new Promise(resolve => {
+              new Promise((resolve) => {
                 setTimeout(() => {
                   resolve();
                   axios
@@ -158,13 +158,13 @@ function Banks(props) {
                         id_bank: newData.id,
                         name_bank: newData.name,
                         city_bank: newData.city,
-                        active: newData.active
+                        active: newData.active,
                       }
                     )
-                    .then(response => {
+                    .then((response) => {
                       console.log(response);
                       if (oldData) {
-                        setState(prevState => {
+                        setState((prevState) => {
                           const data = [...prevState.data];
                           data[data.indexOf(oldData)] = newData;
                           //Message Alert
@@ -176,7 +176,7 @@ function Banks(props) {
                         });
                       }
                     })
-                    .catch(error => {
+                    .catch((error) => {
                       //Message Alert
                       setType("error");
                       setMessaje("All fields are required");
@@ -185,24 +185,24 @@ function Banks(props) {
                       console.log(error);
                     });
                 }, 600);
-              })
+              }),
           }}
         />
-      </Paper>
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        onClose={handleClose}
-        open={open}
-        autoHideDuration={3000}
-      >
-        <SnackbarMesssages
-          variant={type}
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
           onClose={handleClose}
-          message={messaje}
-        />
-      </Snackbar>
+          open={open}
+          autoHideDuration={3000}
+        >
+          <SnackbarMesssages
+            variant={type}
+            onClose={handleClose}
+            message={messaje}
+          />
+        </Snackbar>
+      </Paper>
     </>
   );
 }
 
-export default withStyles(styles)(Banks);
+export default Banks;
